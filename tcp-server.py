770338f800daa3aa263d88d887c01512415d4177
nopnop2002 <hau26280@tree.odn.ne.jp>
2023-12-08 14:24:59 +0900

    Changed to non-blocking mode

--- tcp-server.py
+++ tcp-server.py
@@ -1,36 +1,47 @@
 #!/usr/bin/env python3
 
+import signal
 import socket
+import select
 
 server_ip = "0.0.0.0"
 server_port = 8080
 listen_num = 5
 buffer_size = 1024
 
+def handler(signal, frame):
+	global isRunning
+	#print('handler')
+	isRunning = False
+
 def main():
+	signal.signal(signal.SIGINT, handler)
+
 	print("+==========================+")
 	print("| ESP32 TCP Logging Server |")
 	print("+==========================+")
 	print("")
 
 	tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	tcp_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	tcp_server.bind((server_ip, server_port))
 	tcp_server.listen(listen_num)
 	client,address = tcp_server.accept()
-	print("[*] Connected!! [ Source : {}]".format(address))
+	#print("Connected!! [ Source : {}]".format(address))
+	client.setblocking(0)
 
-	while True:
-		#client,address = tcp_server.accept()
-		#print("[*] Connected!! [ Source : {}]".format(address))
+	global isRunning
+	isRunning = True
+	while isRunning:
+		ready = select.select([client], [], [], 1)
+		#print("ready={}".format(ready[0]))
+		if ready[0]:
+			data = client.recv(buffer_size)
+			if (type(data) is bytes):
+				data = data.decode('utf-8')
+				#print("[*] Received Data : {}".format(data))
+				print(data, end='')
 	
-		data = client.recv(buffer_size)
-		if (type(data) is bytes):
-			data = data.decode('utf-8')
-			#print("[*] Received Data : {}".format(data))
-			print(data, end='')
-	
-		#client.send(b"ACK!!")
-
 	client.close()
 
 if __name__ == "__main__":
